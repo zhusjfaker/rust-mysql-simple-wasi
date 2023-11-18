c2fe17f2ea1ba146d81fd8f01f8e9f4b506e23f3
Zhu Jason <jason.zhu@shopee.com>
2023-11-18 20:03:29 +0800

    feat: first init

---
+++ README.md
@@ -0,0 +1,103 @@
+# Simple mysql driver for WASI 
+
+This crate offers:
+
+*   MySql database driver in pure rust;
+*   connection pool;
+*   Compiles to WebAssembly and runs in the [WasmEdge Runtime](https://github.com/WasmEdge/WasmEdge#readme) as a lightweight alternative to Linux containers;
+*   We do not yet support SSL / TLS connections to databases in the WASI driver
+
+For more details and usage examples, please see the upstream [rust-mysql-simple](https://github.com/blackbeam/rust-mysql-simple) source and [this example](https://github.com/WasmEdge/wasmedge-db-examples/tree/main/mysql).
+
+### Installation
+
+Put the desired version of the crate into the `dependencies` section of your `Cargo.toml`:
+
+```toml
+[dependencies]
+mysql_wasi = "23.0.1"
+```
+
+### Example
+
+```rust
+use mysql::*;
+use mysql::prelude::*;
+
+#[derive(Debug, PartialEq, Eq)]
+struct Payment {
+    customer_id: i32,
+    amount: i32,
+    account_name: Option<String>,
+}
+
+
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let url = "mysql://root:password@localhost:3307/db_name";
+    # Opts::try_from(url)?;
+    # let url = get_opts();
+    let pool = Pool::new(url)?;
+
+    let mut conn = pool.get_conn()?;
+
+    // Let's create a table for payments.
+    conn.query_drop(
+        r"CREATE TEMPORARY TABLE payment (
+            customer_id int not null,
+            amount int not null,
+            account_name text
+        )")?;
+
+    let payments = vec![
+        Payment { customer_id: 1, amount: 2, account_name: None },
+        Payment { customer_id: 3, amount: 4, account_name: Some("foo".into()) },
+        Payment { customer_id: 5, amount: 6, account_name: None },
+        Payment { customer_id: 7, amount: 8, account_name: None },
+        Payment { customer_id: 9, amount: 10, account_name: Some("bar".into()) },
+    ];
+
+    // Now let's insert payments to the database
+    conn.exec_batch(
+        r"INSERT INTO payment (customer_id, amount, account_name)
+          VALUES (:customer_id, :amount, :account_name)",
+        payments.iter().map(|p| params! {
+            "customer_id" => p.customer_id,
+            "amount" => p.amount,
+            "account_name" => &p.account_name,
+        })
+    )?;
+
+    // Let's select payments from database. Type inference should do the trick here.
+    let selected_payments = conn
+        .query_map(
+            "SELECT customer_id, amount, account_name from payment",
+            |(customer_id, amount, account_name)| {
+                Payment { customer_id, amount, account_name }
+            },
+        )?;
+
+    // Let's make sure, that `payments` equals to `selected_payments`.
+    // Mysql gives no guaranties on order of returned rows
+    // without `ORDER BY`, so assume we are lucky.
+    assert_eq!(payments, selected_payments);
+    println!("Yay!");
+
+    Ok(())
+}
+```
+
+## License
+
+Licensed under either of
+
+* Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or https://www.apache.org/licenses/LICENSE-2.0)
+* MIT license ([LICENSE-MIT](LICENSE-MIT) or https://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally
+submitted for inclusion in the work by you, as defined in the Apache-2.0
+license, shall be dual licensed as above, without any additional terms or
+conditions.
---
+++ src/io/mod.rs
@@ -0,0 +1,200 @@
+// Copyright (c) 2020 rust-mysql-simple contributors
+//
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT
+// license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. All files in the project carrying such notice may not be copied,
+// modified, or distributed except according to those terms.
+
+use bufstream::BufStream;
+use io_enum::*;
+#[cfg(windows)]
+use named_pipe as np;
+
+#[cfg(not(target_os = "wasi"))]
+use std::net::{self, SocketAddr};
+#[cfg(unix)]
+use std::os::{
+    unix,
+    unix::io::{AsRawFd, RawFd},
+};
+use std::{fmt, io, time::Duration};
+
+#[cfg(target_os = "wasi")]
+use wasmedge_wasi_socket::{self, SocketAddr};
+
+use crate::error::{
+    DriverError::{ConnectTimeout, CouldNotConnect},
+    Error::DriverError,
+    Result as MyResult,
+};
+
+mod tcp;
+mod tls;
+
+#[derive(Debug, Read, Write)]
+pub enum Stream {
+    #[cfg(unix)]
+    SocketStream(BufStream<unix::net::UnixStream>),
+    #[cfg(windows)]
+    SocketStream(BufStream<np::PipeClient>),
+    TcpStream(TcpStream),
+}
+
+impl Stream {
+    #[cfg(unix)]
+    pub fn connect_socket(
+        socket: &str,
+        read_timeout: Option<Duration>,
+        write_timeout: Option<Duration>,
+    ) -> MyResult<Stream> {
+        match unix::net::UnixStream::connect(socket) {
+            Ok(stream) => {
+                stream.set_read_timeout(read_timeout)?;
+                stream.set_write_timeout(write_timeout)?;
+                Ok(Stream::SocketStream(BufStream::new(stream)))
+            }
+            Err(e) => {
+                let addr = socket.to_string();
+                let desc = e.to_string();
+                Err(DriverError(CouldNotConnect(Some((addr, desc, e.kind())))))
+            }
+        }
+    }
+
+    #[cfg(windows)]
+    pub fn connect_socket(
+        socket: &str,
+        read_timeout: Option<Duration>,
+        write_timeout: Option<Duration>,
+    ) -> MyResult<Stream> {
+        let full_name = format!(r"\\.\pipe\{}", socket);
+        match np::PipeClient::connect(full_name.clone()) {
+            Ok(mut stream) => {
+                stream.set_read_timeout(read_timeout);
+                stream.set_write_timeout(write_timeout);
+                Ok(Stream::SocketStream(BufStream::new(stream)))
+            }
+            Err(e) => {
+                let desc = format!("{}", e);
+                Err(DriverError(CouldNotConnect(Some((
+                    full_name,
+                    desc,
+                    e.kind(),
+                )))))
+            }
+        }
+    }
+
+    // #[cfg(all(not(unix), not(windows)))]
+    // fn connect_socket(&mut self) -> MyResult<()> {
+    //     unimplemented!("Sockets is not implemented on current platform");
+    // }
+
+    pub fn connect_tcp(
+        ip_or_hostname: &str,
+        port: u16,
+        read_timeout: Option<Duration>,
+        write_timeout: Option<Duration>,
+        tcp_keepalive_time: Option<u32>,
+        #[cfg(any(target_os = "linux", target_os = "macos",))]
+        tcp_keepalive_probe_interval_secs: Option<u32>,
+        #[cfg(any(target_os = "linux", target_os = "macos",))] tcp_keepalive_probe_count: Option<
+            u32,
+        >,
+        #[cfg(target_os = "linux")] tcp_user_timeout: Option<u32>,
+        nodelay: bool,
+        tcp_connect_timeout: Option<Duration>,
+        bind_address: Option<SocketAddr>,
+    ) -> MyResult<Stream> {
+        let mut builder = tcp::MyTcpBuilder::new((ip_or_hostname, port));
+        builder
+            .connect_timeout(tcp_connect_timeout)
+            .read_timeout(read_timeout)
+            .write_timeout(write_timeout)
+            .keepalive_time_ms(tcp_keepalive_time)
+            .nodelay(nodelay)
+            .bind_address(bind_address);
+        #[cfg(any(target_os = "linux", target_os = "macos",))]
+        builder.keepalive_probe_interval_secs(tcp_keepalive_probe_interval_secs);
+        #[cfg(any(target_os = "linux", target_os = "macos",))]
+        builder.keepalive_probe_count(tcp_keepalive_probe_count);
+        #[cfg(target_os = "linux")]
+        builder.user_timeout(tcp_user_timeout);
+        builder
+            .connect()
+            .map(|stream| Stream::TcpStream(TcpStream::Insecure(BufStream::new(stream))))
+            .map_err(|err| {
+                if err.kind() == io::ErrorKind::TimedOut {
+                    DriverError(ConnectTimeout)
+                } else {
+                    let addr = format!("{}:{}", ip_or_hostname, port);
+                    let desc = format!("{}", err);
+                    DriverError(CouldNotConnect(Some((addr, desc, err.kind()))))
+                }
+            })
+    }
+
+    pub fn is_insecure(&self) -> bool {
+        false
+    }
+
+    pub fn is_socket(&self) -> bool {
+        false
+    }
+
+    #[cfg(all(not(feature = "native-tls"), not(feature = "rustls")))]
+    pub fn make_secure(self, _host: url::Host, _ssl_opts: crate::SslOpts) -> MyResult<Stream> {
+        panic!(
+            "Client had asked for TLS connection but TLS support is disabled. \
+            Please enable one of the following features: [\"native-tls\", \"rustls\"]"
+        )
+    }
+}
+
+#[cfg(unix)]
+impl AsRawFd for Stream {
+    fn as_raw_fd(&self) -> RawFd {
+        match self {
+            Stream::SocketStream(stream) => stream.get_ref().as_raw_fd(),
+            Stream::TcpStream(stream) => stream.as_raw_fd(),
+        }
+    }
+}
+
+#[derive(Read, Write)]
+pub enum TcpStream {
+    #[cfg(feature = "native-tls")]
+    Secure(BufStream<native_tls::TlsStream<net::TcpStream>>),
+    #[cfg(feature = "rustls")]
+    Secure(BufStream<rustls::StreamOwned<rustls::ClientConnection, net::TcpStream>>),
+    #[cfg(not(target_os = "wasi"))]
+    Insecure(BufStream<net::TcpStream>),
+    #[cfg(target_os = "wasi")]
+    Insecure(BufStream<wasmedge_wasi_socket::TcpStream>),
+}
+
+#[cfg(unix)]
+impl AsRawFd for TcpStream {
+    fn as_raw_fd(&self) -> RawFd {
+        match self {
+            #[cfg(feature = "native-tls")]
+            TcpStream::Secure(stream) => stream.get_ref().get_ref().as_raw_fd(),
+            #[cfg(feature = "rustls")]
+            TcpStream::Secure(stream) => stream.get_ref().get_ref().as_raw_fd(),
+            TcpStream::Insecure(stream) => stream.get_ref().as_raw_fd(),
+        }
+    }
+}
+
+impl fmt::Debug for TcpStream {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            #[cfg(feature = "native-tls")]
+            TcpStream::Secure(ref s) => write!(f, "Secure stream {:?}", s),
+            #[cfg(feature = "rustls")]
+            TcpStream::Secure(ref s) => write!(f, "Secure stream {:?}", s),
+            TcpStream::Insecure(ref s) => write!(f, "Insecure stream {:?}", s),
+        }
+    }
+}
